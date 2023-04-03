8dc0a9e46a79ed09377f587f16cdf1bc934e07c7
Haddad-Alae <118848813+Haddad-Alae@users.noreply.github.com>
2023-04-03 15:31:34 +0000

    Add files via upload

---
+++ Essalaf/src/essalaf/Essalaf.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package essalaf;
+
+/**
+ *
+ * @author EliteBook 840 G4
+ */
+public class Essalaf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
